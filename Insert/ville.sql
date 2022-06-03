33161cb04ea689c0074880d6a73de1956dce7fd6
Brindo RATIHARIMANANA <hei.brindo.2@gmail.com>
2022-06-03 08:42:28 +0300

    ajour du dossier insert(chauffeur,client,vehicule,ville)

---
+++ Insert/ville.sql
@@ -0,0 +1,6 @@
+insert into ville(nom_ville) values('Antananarivo');
+insert into ville(nom_ville) values('Antsirabe');
+insert into ville(nom_ville) values('Fianarantsoa');
+insert into ville(nom_ville) values('Toamasina');
+insert into ville(nom_ville) values('Morondava');
+insert into ville(nom_ville) values('Diego');
