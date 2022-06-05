73f32ce3f647a5b5749c63532c5618fee24502df
Ny Ando Mayah <hei.mayah.3@gmail.com>
2022-06-05 15:48:22 +0300

    feat: adding vehicules.sql in inserts_for_the_database

---
+++ inserts_for_the_database/vehicules.sql
@@ -0,0 +1,8 @@
+insert into MOCK_DATA (matricule, nb_places, status) values ('4851TGR', '10', true);
+insert into MOCK_DATA (matricule, nb_places, status) values ('3627TKQ', '10', true);
+insert into MOCK_DATA (matricule, nb_places, status) values ('6863TXT', '10', true);
+insert into MOCK_DATA (matricule, nb_places, status) values ('8530TKQ', '10', true);
+insert into MOCK_DATA (matricule, nb_places, status) values ('0231TPQ', '10', true);
+insert into MOCK_DATA (matricule, nb_places, status) values ('3298TVZ', '16', false);
+insert into MOCK_DATA (matricule, nb_places, status) values ('6932TCR', '16', false);
+insert into MOCK_DATA (matricule, nb_places, status) values ('3218TLA', '16', false);
