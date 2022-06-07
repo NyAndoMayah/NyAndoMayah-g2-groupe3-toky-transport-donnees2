7bd48a10f0062268cc35ef86270beb077e0a45af
Ny Ando Mayah <hei.mayah.3@gmail.com>
2022-06-07 18:59:46 +0300

    fix: add changes to vehicules.sql

--- inserts_for_the_database/vehicules.sql
+++ inserts_for_the_database/vehicules.sql
@@ -1,8 +1,8 @@
-insert into MOCK_DATA (matricule, nb_places, status) values ('4851TGR', '10', true);
-insert into MOCK_DATA (matricule, nb_places, status) values ('3627TKQ', '10', true);
-insert into MOCK_DATA (matricule, nb_places, status) values ('6863TXT', '10', true);
-insert into MOCK_DATA (matricule, nb_places, status) values ('8530TKQ', '10', true);
-insert into MOCK_DATA (matricule, nb_places, status) values ('0231TPQ', '10', true);
-insert into MOCK_DATA (matricule, nb_places, status) values ('3298TVZ', '16', false);
-insert into MOCK_DATA (matricule, nb_places, status) values ('6932TCR', '16', false);
-insert into MOCK_DATA (matricule, nb_places, status) values ('3218TLA', '16', false);
+INSERT INTO mytable(matricule,nb_places) VALUES ('3542TBR',10);
+INSERT INTO mytable(matricule,nb_places) VALUES ('0905TBD',10);
+INSERT INTO mytable(matricule,nb_places) VALUES ('5429TBD',10);
+INSERT INTO mytable(matricule,nb_places) VALUES ('7318TBN',10);
+INSERT INTO mytable(matricule,nb_places) VALUES ('8977TBK',10);
+INSERT INTO mytable(matricule,nb_places) VALUES ('3723TBQ',16);
+INSERT INTO mytable(matricule,nb_places) VALUES ('3223TBR',16);
+INSERT INTO mytable(matricule,nb_places) VALUES ('5387TBX',16);
