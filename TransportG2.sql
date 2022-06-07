4a40e6bb1ec83d2b16370d4a4736efa385dee885
Ny Ando Mayah <hei.mayah.3@gmail.com>
2022-06-07 23:15:30 +0300

    feat: add TransportG2.sql

---
+++ TransportG2.sql
@@ -0,0 +1,92 @@
+CREATE DATABASE toky_trans2;
+
+\c toky_trans2;
+-- Table chauffeur -- done
+CREATE TABLE chauffeur (
+    id_chauffeur serial primary key, 
+    nom varchar(150) not null,
+    prenom varchar(150),
+    cin varchar(12),
+    contact varchar(15),
+    email varchar(150) not null
+);
+
+-- Table conduire --done
+CREATE TABLE conduire(
+    id_chauffeur int not null REFERENCES chauffeur(id_chauffeur) not null,
+    id_vehicule varchar(2) not null REFERENCES vehicule(id_vehicule) --alter table here
+);
+
+-- Table vehicule --done
+CREATE TABLE vehicule(
+    id_vehicule serial primary key, --alter table here
+    matricule varchar(8) primary key,
+    nb_place int not null, 
+    status boolean
+);
+-- Table depense --done
+CREATE TABLE depense(
+    id_depense serial primary key, 
+    essence float, 
+    maintenance int
+);
+
+-- Table recevoir --done
+CREATE TABLE recevoir(
+    id_vehicule varchar(2) not null REFERENCES vehicule(id_vehicule), --alter table here
+    id_depense int not null REFERENCES depense(id_depense)
+);
+
+
+-- Table client --done
+CREATE TABLE client(
+    id_client bigserial primary key,
+    nom varchar(150) not null,
+    prenom varchar(150),
+    cin varchar(12),
+    contact varchar(15)
+);
+
+-- Table offre --done
+CREATE TABLE offre(
+  id_offre serial primary key,
+  tarif int not null check(tarif > 0),
+  label varchar(100) not null
+);
+
+-- Table reserver --doing it
+CREATE TABLE reserver(
+    id_client int not null REFERENCES client(id_client),
+    id_voyage int REFERENCES voyage(id_voyage),
+    date_reservation date DEFAULT current_date,
+    place int not null, 
+    montant_paye int check (montant_paye > 0) DEFAULT 0
+);
+
+-- Table voyage --done
+CREATE TABLE voyage(
+    id_voyage bigserial primary key,
+    date_voyage date not null, 
+    heure_dep time not null, 
+    nb_place_dispo int check(nb_place_dispo >= 0), --changes done here with check
+    id_offre int REFERENCES offre(id_offre), 
+    matricule varchar(8) REFERENCES vehicule(matricule),
+    id_ville_depart int not null REFERENCES ville(id_ville),
+    id_ville_arrivee int not null REFERENCES ville(id_ville)
+);
+
+-- Table ville --done
+CREATE TABLE ville(
+    id_ville serial primary key,
+    nom_ville varchar(100) not null
+);
+
+
+
+
+
+
+
+
+
+
