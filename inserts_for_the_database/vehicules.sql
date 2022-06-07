7a6556365434468e78636b05be2fb15e97d0fc3d
Ny Ando Mayah <hei.mayah.3@gmail.com>
2022-06-07 22:11:29 +0300

    fix : replacing mytable by the real name of the table in all the files

--- inserts_for_the_database/vehicules.sql
+++ inserts_for_the_database/vehicules.sql
@@ -1,9 +1,9 @@
-INSERT INTO mytable(id_vehicule,matricule,nb_place) VALUES (1,'3564TBT',16);
-INSERT INTO mytable(id_vehicule,matricule,nb_place) VALUES (2,'5079TBQ',16);
-INSERT INTO mytable(id_vehicule,matricule,nb_place) VALUES (3,'1714TBD',10);
-INSERT INTO mytable(id_vehicule,matricule,nb_place) VALUES (4,'2809TBH',10);
-INSERT INTO mytable(id_vehicule,matricule,nb_place) VALUES (5,'0272TBV',16);
-INSERT INTO mytable(id_vehicule,matricule,nb_place) VALUES (6,'3729TBY',10);
-INSERT INTO mytable(id_vehicule,matricule,nb_place) VALUES (7,'7816TBN',10);
-INSERT INTO mytable(id_vehicule,matricule,nb_place) VALUES (8,'5411TBI',16);
+INSERT INTO vehicule(id_vehicule,matricule,nb_place) VALUES (1,'3564TBT',16);
+INSERT INTO vehicule(id_vehicule,matricule,nb_place) VALUES (2,'5079TBQ',16);
+INSERT INTO vehicule(id_vehicule,matricule,nb_place) VALUES (3,'1714TBD',10);
+INSERT INTO vehicule(id_vehicule,matricule,nb_place) VALUES (4,'2809TBH',10);
+INSERT INTO vehicule(id_vehicule,matricule,nb_place) VALUES (5,'0272TBV',16);
+INSERT INTO vehicule(id_vehicule,matricule,nb_place) VALUES (6,'3729TBY',10);
+INSERT INTO vehicule(id_vehicule,matricule,nb_place) VALUES (7,'7816TBN',10);
+INSERT INTO vehicule(id_vehicule,matricule,nb_place) VALUES (8,'5411TBI',16);
 
