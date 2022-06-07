13cf97ed91071a22ef2dd4c9c0198cb5b4ae16c7
Ny Ando Mayah <hei.mayah.3@gmail.com>
2022-06-07 22:50:13 +0300

    feat: add voyages.sql

--- inserts_for_the_database/villes.sql
+++ inserts_for_the_database/villes.sql
@@ -3,4 +3,4 @@ insert into ville(id_ville,nom_ville) values(2,'Antsirabe');
 insert into ville(id_ville,nom_ville) values(3,'Fianarantsoa');
 insert into ville(id_ville,nom_ville) values(4,'Toamasina');
 insert into ville(id_ville,nom_ville) values(5,'Morondava');
-insert into ville(id_ville,nom_ville) values(6,'Diego');
+insert into ville(id_ville,nom_ville) values(6,'Antsiranana');
