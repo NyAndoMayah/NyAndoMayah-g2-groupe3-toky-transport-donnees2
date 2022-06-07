8097fef34261cf456afbfdf675a9037ad0814faf
Brindo Ratiharimanana <98638794+BrindoTolotra@users.noreply.github.com>
2022-06-07 10:23:53 +0300

    J'ai changé le dernier du matricule en "T##"

--- inserts_for_the_database/vehicules.sql
+++ inserts_for_the_database/vehicules.sql
@@ -9,15 +9,15 @@ insert into MOCK_DATA (matricule, nb_places, status) values ('3218TLA', '16', fa
 
 -- je pense que 8 vehicules ne sont pas assez pour le base 
 -- j'ajoute 5 vehicules de 10 place et 7 vehicule de 16 place
-insert into vehicule (matricule, nb_places, status) values ('9553JBX', 10, false);
-insert into vehicule (matricule, nb_places, status) values ('1667PKA', 10, true);
-insert into vehicule (matricule, nb_places, status) values ('7982PFY', 10, false);
-insert into vehicule (matricule, nb_places, status) values ('6279MQC', 10, true);
-insert into vehicule (matricule, nb_places, status) values ('0894RAK', 10, true);
-insert into vehicule (matricule, nb_places, status) values ('1433ANM', 16, false);
-insert into vehicule (matricule, nb_places, status) values ('7203WRQ', 16, true);
-insert into vehicule (matricule, nb_places, status) values ('5333UGC', 16, true);
-insert into vehicule (matricule, nb_places, status) values ('1923FUN', 16, false);
-insert into vehicule (matricule, nb_places, status) values ('1872JIZ', 16, false);
-insert into vehicule (matricule, nb_places, status) values ('9128EOL', 16, true);
-insert into vehicule (matricule, nb_places, status) values ('0025RJE', 16, true);
+insert into vehicule (matricule, nb_places, status) values ('9553TBX', 10, false);
+insert into vehicule (matricule, nb_places, status) values ('1667TKA', 10, true);
+insert into vehicule (matricule, nb_places, status) values ('7982TFY', 10, false);
+insert into vehicule (matricule, nb_places, status) values ('6279TQC', 10, true);
+insert into vehicule (matricule, nb_places, status) values ('0894TAK', 10, true);
+insert into vehicule (matricule, nb_places, status) values ('1433TNM', 16, false);
+insert into vehicule (matricule, nb_places, status) values ('7203TRQ', 16, true);
+insert into vehicule (matricule, nb_places, status) values ('5333TGC', 16, true);
+insert into vehicule (matricule, nb_places, status) values ('1923TUN', 16, false);
+insert into vehicule (matricule, nb_places, status) values ('1872TIZ', 16, false);
+insert into vehicule (matricule, nb_places, status) values ('9128TOL', 16, true);
+insert into vehicule (matricule, nb_places, status) values ('0025TJE', 16, true);
