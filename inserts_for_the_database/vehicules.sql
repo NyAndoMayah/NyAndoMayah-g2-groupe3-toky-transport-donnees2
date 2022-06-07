d572c6393f57435cfbe491bc4358c6472e3b9936
Brindo Ratiharimanana <98638794+BrindoTolotra@users.noreply.github.com>
2022-06-07 09:49:04 +0300

    ajout des autres vehicules

--- inserts_for_the_database/vehicules.sql
+++ inserts_for_the_database/vehicules.sql
@@ -6,3 +6,18 @@ insert into MOCK_DATA (matricule, nb_places, status) values ('0231TPQ', '10', tr
 insert into MOCK_DATA (matricule, nb_places, status) values ('3298TVZ', '16', false);
 insert into MOCK_DATA (matricule, nb_places, status) values ('6932TCR', '16', false);
 insert into MOCK_DATA (matricule, nb_places, status) values ('3218TLA', '16', false);
+
+-- je pense que 8 vehicules ne sont pas assez pour le base 
+-- j'ajoute 5 vehicules de 10 place et 7 vehicule de 16 place
+insert into vehicule (matricule, nb_places, status) values ('9553JBX', 10, false);
+insert into vehicule (matricule, nb_places, status) values ('1667PKA', 10, true);
+insert into vehicule (matricule, nb_places, status) values ('7982PFY', 10, false);
+insert into vehicule (matricule, nb_places, status) values ('6279MQC', 10, true);
+insert into vehicule (matricule, nb_places, status) values ('0894RAK', 10, true);
+insert into vehicule (matricule, nb_places, status) values ('1433ANM', 16, false);
+insert into vehicule (matricule, nb_places, status) values ('7203WRQ', 16, true);
+insert into vehicule (matricule, nb_places, status) values ('5333UGC', 16, true);
+insert into vehicule (matricule, nb_places, status) values ('1923FUN', 16, false);
+insert into vehicule (matricule, nb_places, status) values ('1872JIZ', 16, false);
+insert into vehicule (matricule, nb_places, status) values ('9128EOL', 16, true);
+insert into vehicule (matricule, nb_places, status) values ('0025RJE', 16, true);
