f1d7a4b2796cf243533f44c33ac8422b7632b0e8
Brindo RATIHARIMANANA <hei.brindo.2@gmail.com>
2022-06-09 09:13:19 +0300

    update

--- Insert/ville.sql
+++
@@ -1,6 +0,0 @@
-insert into ville(nom_ville) values('Antananarivo');
-insert into ville(nom_ville) values('Antsirabe');
-insert into ville(nom_ville) values('Fianarantsoa');
-insert into ville(nom_ville) values('Toamasina');
-insert into ville(nom_ville) values('Morondava');
-insert into ville(nom_ville) values('Diego');
