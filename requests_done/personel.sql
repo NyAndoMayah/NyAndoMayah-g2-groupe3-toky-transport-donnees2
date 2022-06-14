72dd25b4ac835aa51ae0be2e60e94df89e67059d
Bryan Lakara <98728691+Lakara2@users.noreply.github.com>
2022-06-14 19:56:04 +0300

    Delete personel.sql

--- requests_done/personel.sql
+++
@@ -1,5 +0,0 @@
---le nombre de voyage qu'une voiture a fait durant un delai determiner 
-SELECT count(matricule),voit.id_vehicule AS voiture FROM vehicule 
-INNER JOIN voyage AS voit ON vehicule.id_vehicule = voit.id_vehicule 
-WHERE voit.id_vehicule =5 
-AND  date_voyage BETWEEN '2021-11-10' AND '2021-11-25' GROUP BY voit.id_vehicule;
