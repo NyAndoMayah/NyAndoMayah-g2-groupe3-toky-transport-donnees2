8c89a09abd9cf7bf5953678397a43a19b5cac22d
Bryan Lakara <98728691+Lakara2@users.noreply.github.com>
2022-06-14 20:15:29 +0300

    Delete requests_done/HARD directory

--- requests_done/HARD/HARD -line43.sql
+++
@@ -1,5 +0,0 @@
---depense des vehicules apres un certain nombre de voyage fait HARD -line 43
-SELECT sum(essence+maintenance) AS Depense, matricule ,count(matricule) AS nb_voyage_fait FROM vehicule INNER JOIN recevoir ON vehicule.id_vehicule = recevoir.id_vehicule
-INNER JOIN depense ON depense.id_depense = recevoir.id_depense
-INNER JOIN voyage ON vehicule.id_vehicule = voyage.id_vehicule
-WHERE vehicule.id_vehicule = 5 AND date_voyage BETWEEN '2021-11-10' AND '2021-11-15';
