e3b7f82c63fbbad8e910fa08b568ecd2505f75b7
Bryan Lakara <98728691+Lakara2@users.noreply.github.com>
2022-06-23 10:42:36 +0300

    Update request snd.sql

--- requests_done/request snd.sql
+++ requests_done/request snd.sql
@@ -41,7 +41,7 @@ WHERE montant_paye = tarif AND date_voyage = current_date;
 SELECT sum( montant_paye)  FROM reserver WHERE date_reservation BETWEEN '10-11-2021' AND current_date;
 
 --depense des vehicules apres un certain nombre de voyage fait HARD -line 43
-SELECT sum(essence+maintenance) AS Depense, matricule ,count(matricule) AS nb_voyage_fait FROM vehicule INNER JOIN recevoir ON vehicule.id_vehicule = recevoir.id_vehicule
+SELECT sum(essence+maintenance) AS Depense, count(matricule) AS nb_voyage_fait FROM vehicule INNER JOIN recevoir ON vehicule.id_vehicule = recevoir.id_vehicule
 INNER JOIN depense ON depense.id_depense = recevoir.id_depense
 INNER JOIN voyage ON vehicule.id_vehicule = voyage.id_vehicule
 WHERE vehicule.id_vehicule = 5 AND date_voyage BETWEEN '2021-11-10' AND '2021-11-15';
